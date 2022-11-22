c9896c9eb05c4c16e0ca9a7c1f693aa15e102dcd
JomarMonreal <jpmonreal2035@gmail.com>
2022-11-22 15:46:37 +0800

    page cover navigation pane src files

---
+++ src/PageCover/PageCoverNavigationPane/PageCoverNavigationPane.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './pageCoverNavigationPane.css'
+
+
+const PageCoverNavigationPane = () => {  
+
+  return (
+    <div className='pageCoverNavigationPane'>
+    </div>
+  )
+}
+
+
+
+export default PageCoverNavigationPane
